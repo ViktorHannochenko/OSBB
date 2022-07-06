b209da8261de7e0154978ba68c609be3e34e896d
email <ukrvictor1987@gmail.com>
2022-07-06 15:07:58 +0300

    3_OSBB

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 window.addEventListener('DOMContentLoaded', () => {
     // Меню гамбургер
     const hamburger = document.querySelector('.hamburger'),
@@ -133,25 +131,24 @@ $(document).ready(function () {
 
     $('input[name=phone]').mask("+38 (999) 999-99-99");
 
-    // $('form').submit(function(e) {
-    //     e.preventDefault();
 
-    //     if(!$(this).valid()) {
-    //         return;
-    //     }
-
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "mailer/smart.php",
-    //         data: $(this).serialize()
-    //     }).done(function() {
-    //         $(this).find("input").val("");
-    //         $('#consultation, #order').fadeOut();
-    //         $('.overlay, #thanks').fadeIn('slow');
-
-    //         $('form').trigger('reset');
-    //     });
-    //     return false;
-    // });
+    $('form').submit(function(e) {
+        e.preventDefault();
+    
+        if (!$(this).valid()) {
+            return;
+        }
+    
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+    
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 
 });
