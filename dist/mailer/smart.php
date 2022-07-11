450848e42ec6074a7def34b8b811edc423eb06fc
email <ukrvictor1987@gmail.com>
2022-07-11 13:15:44 +0300

    5_OSBB

---
+++ dist/mailer/smart.php
@@ -0,0 +1,46 @@
+<?php 
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$text = $_POST['text'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'frontendukraine1987';                 // Наш логин
+$mail->Password = 'apqmacxtbqbiywkg';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('frontendukraine1987', 'Portfolio');   // От кого письмо 
+$mail->addAddress('vhannochenko@ukr.net');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	E-mail: ' . $email . '<br>
+	Номер телефона: ' . $phone . '<br>
+	Сообщение: ' . $text .'';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
--- gulpfile.js
+++ gulpfile.js
@@ -67,5 +67,10 @@ gulp.task('images', function () {
         .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
 });
+gulp.task('pdf', function () {
+    return gulp.src("src/documents/**/*.pdf")
+        .pipe(gulp.dest("dist/documents"))
+        .pipe(browserSync.stream());
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images', 'pdf'));
